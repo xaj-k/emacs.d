290a09e73596f7fd64ec98e4b15f5bd435391807
Kevin Jackson <kevinjackson91@gmail.com>
2019-05-02 14:41:44 -0600

    removed everything from fork

--- site-lisp/nispio/midas-config.el
+++
@@ -1,6 +0,0 @@
-
-(eval-after-load 'semantic
-  (progn
-	(semantic-add-system-include (substitute-in-file-name "$XMDISK/xm/inc"))
-	(semantic-add-system-include (substitute-in-file-name "$XMDISK/xm/include/midas"))
-	nil))
